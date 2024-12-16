439b75c87983c8a097067c958575a7e7ac35abb0
TheNiister <niiokai.tettey@gmail.com>
2024-12-16 13:06:18 +0000

    LTDK level testing and AI upgrades
    added classes, tweaked ai for adventures and enemies. Added a test enviroment to test pathfinding and importing from ldtk

--- Assets/Scripts/Questing/HitPointManager.cs
+++ Assets/Scripts/Questing/HitPointManager.cs
@@ -15,7 +15,22 @@ public class HitPointManager : MonoBehaviour
 
     void Update()
     {
-        if(currentHP <= 0)
+
+    }
+
+    public void EffectHealth(float value)
+    {
+        currentHP -= value;
+
+        if(currentHP > maxHP)
+            currentHP = maxHP;
+
+        if(gameObject.GetComponent<NPC_AdventurerTargeting>())
+        {
+            gameObject.GetComponent<NPC_AdventurerTargeting>().FindTarget();
+        }
+
+        if (currentHP <= 0)
         {
             Destroy(gameObject);
         }
---
+++ Assets/Scripts/Questing/Party/NPC/AttackScripts/Scripts/NPC_ArcherAttack.cs
@@ -0,0 +1,74 @@
+using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+[CreateAssetMenu(fileName = "Archer", menuName = "NPC_AdventureAttack/Archer")]
+public class NPC_ArcherAttack : NPC_AdventurerAttack
+{
+    public float attackRange;
+    public float attackRate;
+    public float attackDamge = 1;
+    public float fleeRange;
+    public float fleeSpeed = 1;
+
+    private float attackCooldown;
+
+    public override void StartAttack()
+    {
+        
+    }
+
+    public override void UpdateAttack()
+    {
+
+        if (aTarget.currentTarget != null)
+        {
+            float dist = (aTarget.currentTarget.transform.position - thisAdventurer.transform.position).magnitude;
+
+            if (dist < attackRange && attackCooldown <= 0)
+            {
+                AttackTarget();
+                attackCooldown = attackRate;
+            }
+
+            if (dist < attackRange)
+            {
+                if(dist < fleeRange && attackCooldown >= 0)
+                {
+                    thisAdventurer.GetComponent<AIPath>().canMove = false;
+                    thisAdventurer.GetComponent<AIPath>().maxSpeed = 1;
+                    thisAdventurer.transform.position = Vector2.MoveTowards(thisAdventurer.transform.position, aTarget.currentTarget.transform.position, fleeSpeed * - 0.001f);
+                }
+                else
+                {
+                    thisAdventurer.GetComponent<AIPath>().canMove = true;
+                    thisAdventurer.GetComponent<AIPath>().maxSpeed = 0f;
+                }
+                
+            }
+            else
+            {
+                thisAdventurer.GetComponent<AIPath>().canMove = true;
+                thisAdventurer.GetComponent<AIPath>().maxSpeed = 1;
+            }
+
+        }
+
+        if (attackCooldown > 0)
+        {
+            attackCooldown -= 1 * Time.deltaTime;
+        }
+    }
+
+    public override void EndAttack() 
+    {
+        
+    }
+
+    public void AttackTarget()
+    {
+        aTarget.currentTarget.GetComponent<HitPointManager>().currentHP -= attackDamge;
+    }
+}
