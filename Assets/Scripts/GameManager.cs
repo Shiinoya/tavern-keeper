3b13380051980463d7bc8f1618d7654fb23b81c7
Shiinoya <shiinoya94@gmail.com>
2024-12-19 07:59:48 -0700

    Allow left click and register game object if present

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
+
+    Camera Camera;
+    public Unit ActiveUnit;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Mouse mouse = Mouse.current;
+
+        // if mouse left (select)
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector2 mousePosition2D = Camera.ScreenToWorldPoint(mouse.position.ReadValue());
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                // clicked object
+                GameObject clickedObject = hit.collider.gameObject;
+                Debug.Log("You clicked on: " + clickedObject.name);
+            }
+        }
     }
 }
