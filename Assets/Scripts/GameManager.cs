08fdb8c3a0baf61cb7693f6935e534d8d2c57a8d
Shiinoya <shiinoya94@gmail.com>
2024-12-19 08:11:14 -0700

    Clean up GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,18 +16,26 @@ public class GameManager : MonoBehaviour
     {
         Mouse mouse = Mouse.current;
 
-        // if mouse left (select)
+        // if mouse left
         if (mouse.leftButton.wasPressedThisFrame)
         {
             Vector2 mousePosition2D = Camera.ScreenToWorldPoint(mouse.position.ReadValue());
 
             RaycastHit2D hit = Physics2D.Raycast(mousePosition2D, Vector2.zero);
 
-            // if clicked object
+            // if object clicked (select)
             if (hit.collider != null)
             {
                 GameObject clickedObject = hit.collider.gameObject;
-                Debug.Log("Clicked: " + clickedObject.name);
+                Debug.Log("Object clicked: " + clickedObject.name);
+                // TODO : select object
+            }
+
+            // if no object clicked (deselect)
+            if (hit.collider != null)
+            {
+                Debug.Log("No object clicked");
+                // TODO : deselect object
             }
         }
     }
